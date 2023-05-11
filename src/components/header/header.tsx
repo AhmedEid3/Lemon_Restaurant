1548a81d4a8be996da0af8f1eaa4915bd60d1ec5
AhmedEid3 <ahmed.eid3@outlook.com>
2023-05-11 07:24:25 +0300

    Add Header Component - Add Logo Component - Add Nav Component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+import Logo from '../logo/logo';
+import Nav from '../nav/nav';
+
+const Header = () => {
+  return (
+    <header>
+      <Logo />
+      <Nav />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/nav/nav.tsx
@@ -0,0 +1,16 @@
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>home</li>
+        <li>about </li>
+        <li>menu</li>
+        <li>reservations</li>
+        <li>order online</li>
+        <li>login</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
