883774e36246d37b658e3bf3216b0c19e797ee47
AhmedEid3 <ahmed.eid3@outlook.com>
2023-05-13 02:12:15 +0300

    Add purgecss package

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,15 @@
 import presetEnv from 'postcss-preset-env';
 import flexBugsFixes from 'postcss-flexbugs-fixes';
 import postcssNormalize from 'postcss-normalize';
+import purgecss from '@fullhuman/postcss-purgecss';
 
 export default {
-  plugins: [presetEnv({ stage: 1 }), flexBugsFixes(), postcssNormalize()],
+  plugins: [
+    presetEnv({ stage: 1 }),
+    flexBugsFixes(),
+    postcssNormalize(),
+    purgecss({
+      content: ['./**/*.html', './src/**/*.jsx', './src/**/*.tsx'],
+    }),
+  ],
 };
