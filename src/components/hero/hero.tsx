e399cc2b7c00ab07ba69e8167e8cecec4ac56dcf
AhmedEid3 <ahmed.eid3@outlook.com>
2023-05-13 13:52:56 +0300

    Implement Hero Component

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,26 +1,36 @@
 import restaurantFood from '../../assets/restaurant_food.jpg';
+import Button from '../button/Button';
+import './hero.css';
 
 const Hero = () => {
   return (
-    <section>
-      <div>
-        <header>
-          <h2>Little Lemon</h2>
-          <p>Chicago</p>
-        </header>
-        <p>
-          We are a family owned Mediterranean restaurant, focused on traditional
-          recipes served with a modern twist.
-        </p>
-        <button>reserve a table</button>
-      </div>
+    <section className="bg-primary pt-2">
+      <div className="container">
+        <div className="row justify-content-between">
+          <div className="col-md-6 col-lg-4">
+            <header>
+              <h1 className="heading text-secondary">Little Lemon</h1>
+              <p className="subtitle text-light">Chicago</p>
+            </header>
+            <p className="lead text-light">
+              We are a family owned Mediterranean restaurant, focused on
+              traditional recipes served with a modern twist.
+            </p>
+            <div className="mt-5">
+              <Button>Reserve a Table</Button>
+            </div>
+          </div>
 
-      <div>
-        <img
-          style={{ maxWidth: '100%', height: 'auto' }}
-          src={restaurantFood}
-          alt="Highlights"
-        />
+          <div className="col-auto">
+            <div className="container-img rounded">
+              <img
+                className="img-fluid rounded"
+                src={restaurantFood}
+                alt="Highlights"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
