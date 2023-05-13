b8f089f38b69226cb8eb87d8623db68ce5e66f0a
AhmedEid3 <ahmed.eid3@outlook.com>
2023-05-13 13:51:39 +0300

    Implement Nav Component

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,13 +1,44 @@
+import { PropsWithChildren } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+import './nav.style.css';
+
+interface Props extends PropsWithChildren {
+  link: string;
+}
+
+const NavItem = ({ children, link }: Props) => {
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+    navigate(link);
+  };
+
+  return (
+    <li>
+      <div
+        role="link"
+        tabIndex={0}
+        onClick={() => handleNavigation()}
+        onKeyDown={() => handleNavigation()}
+        className="navbar__link"
+      >
+        {children}
+      </div>
+    </li>
+  );
+};
+
 const Nav = () => {
   return (
     <nav>
-      <ul>
-        <li>home</li>
-        <li>about </li>
-        <li>menu</li>
-        <li>reservations</li>
-        <li>order online</li>
-        <li>login</li>
+      <ul className="navbar">
+        <NavItem link="/">home</NavItem>
+        <NavItem link="/about">about</NavItem>
+        <NavItem link="/menu">menu</NavItem>
+        <NavItem link="/reservations">reservations</NavItem>
+        <NavItem link="/booking">order online</NavItem>
+        <NavItem link="/login">login</NavItem>
       </ul>
     </nav>
   );
