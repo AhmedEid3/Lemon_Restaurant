6c881ed896a1e5fe682a6ac220fbd95d86153e04
AhmedEid3 <ahmed.eid3@outlook.com>
2023-05-13 13:58:56 +0300

    Implement Footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,68 +1,155 @@
+import { useNavigate } from 'react-router-dom';
 import Logo from '../logo/logo';
+import './footer.css';
 
 const Footer = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = (link: string) => {
+    navigate(link);
+  };
+
   return (
-    <footer>
-      <div>
-        <Logo />
+    <footer className="py-3">
+      <div className="container">
+        <div className="row justify-content-between">
+          <div
+            className="col-sm-6 col-md-6 col-lg-2"
+            style={{ alignSelf: 'center' }}
+          >
+            <Logo />
+          </div>
+          <section className="col-sm-6 col-md-6 col-lg-3">
+            <h2 className="footer-title">Doormat Navigation</h2>
+            <ul className="footer__menu">
+              <li>
+                <div
+                  tabIndex={0}
+                  role="link"
+                  className="footer__menu__item"
+                  onClick={() => handleNavigation('/')}
+                  onKeyDown={() => handleNavigation('/')}
+                >
+                  Home
+                </div>
+              </li>
+              <li>
+                <div
+                  tabIndex={0}
+                  role="link"
+                  className="footer__menu__item"
+                  onClick={() => handleNavigation('/about')}
+                  onKeyDown={() => handleNavigation('/about')}
+                >
+                  About
+                </div>
+              </li>
+              <li>
+                <div
+                  tabIndex={0}
+                  role="link"
+                  className="footer__menu__item"
+                  onClick={() => handleNavigation('/menu')}
+                  onKeyDown={() => handleNavigation('/menu')}
+                >
+                  menu
+                </div>
+              </li>
+              <li>
+                <div
+                  tabIndex={0}
+                  role="link"
+                  className="footer__menu__item"
+                  onClick={() => handleNavigation('/reservations')}
+                  onKeyDown={() => handleNavigation('/reservations')}
+                >
+                  Reservations
+                </div>
+              </li>
+              <li>
+                <div
+                  tabIndex={0}
+                  role="link"
+                  className="footer__menu__item"
+                  onClick={() => handleNavigation('/booking')}
+                  onKeyDown={() => handleNavigation('/booking')}
+                >
+                  Order Online
+                </div>
+              </li>
+              <li>
+                <div
+                  tabIndex={0}
+                  role="link"
+                  className="footer__menu__item"
+                  onClick={() => handleNavigation('/login')}
+                  onKeyDown={() => handleNavigation('/login')}
+                >
+                  login
+                </div>
+              </li>
+            </ul>
+          </section>
+
+          <section className="col-sm-6 col-md-6 col-lg-5">
+            <h2 className="footer-title">Contact</h2>
+            <ul className="footer__menu">
+              <li className="footer__menu__item">
+                25 High Street | City: Ashford | EX31 1QY | United Kingdom
+              </li>
+              <li>
+                <a className="footer__menu__item" href="tel:+441632960421">
+                  +44 1632 960421
+                </a>
+              </li>
+              <li>
+                <a
+                  className="footer__menu__item"
+                  href="mailto:contact@restaurant-lemon.com"
+                >
+                  contact@restaurant-lemon.com
+                </a>
+              </li>
+            </ul>
+          </section>
+
+          <section className="col-sm-6 col-md-6 col-lg-2">
+            <h2 className="footer-title">Social Media</h2>
+            <ul className="footer__menu">
+              <li>
+                <a
+                  className="footer__menu__item"
+                  href="https://www.facebook.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a
+                  className="footer__menu__item"
+                  href="https://www.youtube.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Youtube
+                </a>
+              </li>
+              <li>
+                <a
+                  className="footer__menu__item"
+                  href="https://www.instagram.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Instagram
+                </a>
+              </li>
+            </ul>
+          </section>
+        </div>
       </div>
-      <section>
-        <h2>Doormat Navigation</h2>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>menu</li>
-          <li>Reservations</li>
-          <li>Order Online</li>
-          <li>Login</li>
-        </ul>
-      </section>
-
-      <section>
-        <h2>Contact</h2>
-        <ul>
-          <li>
-            Address: 25 High Street | City: Ashford | EX31 1QY | United Kingdom
-          </li>
-          <li>
-            <a href="tel:+441632960421">+44 1632 960421</a>
-          </li>
-          <li>
-            <a href="mailto:contact@restaurant-lemon.com">
-              Email: contact@restaurant-lemon.com
-            </a>
-          </li>
-        </ul>
-      </section>
-
-      <section>
-        <h2>Social Media</h2>
-        <ul>
-          <li>
-            <a
-              href="https://www.facebook.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Facebook
-            </a>
-          </li>
-          <li>
-            <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
-              Youtube
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Instagram
-            </a>
-          </li>
-        </ul>
-      </section>
     </footer>
   );
 };
