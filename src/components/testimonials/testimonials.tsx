193a0486275cb2a673743ea53e037c7c59e68925
AhmedEid3 <ahmed.eid3@outlook.com>
2023-05-13 13:54:10 +0300

    Implement Testimonials Component

--- src/components/testimonials/testimonials.tsx
+++ src/components/testimonials/testimonials.tsx
@@ -1,38 +1,50 @@
 import { useState } from 'react';
 import ReviewType from '../review/review.type';
 import Review from '../review/review';
-import userProfileImg from '../../assets/user-profile.svg';
+import alexanderImg from '../../assets/alexander.jpg';
+import darshanImg from '../../assets/darshan.jpg';
+import jacksonImg from '../../assets/jackson.jpg';
+import mandyImg from '../../assets/mandy.jpg';
 
 const Testimonials = () => {
   const [reviews] = useState<Array<ReviewType>>([
     {
       rating: 4,
-      reviewText: 'very Good restaurant',
-      user: { imgLink: userProfileImg, userName: 'Ahmed Eid' },
+      reviewText: 'Very Good restaurant',
+      user: { imgLink: alexanderImg, userName: 'Alexander John' },
     },
     {
       rating: 3.5,
       reviewText: 'Good restaurant',
-      user: { imgLink: userProfileImg, userName: 'Dalia Mahmoud' },
+      user: { imgLink: darshanImg, userName: 'Darshan Jemi' },
     },
     {
       rating: 5,
       reviewText: 'Excellent restaurant',
-      user: { imgLink: userProfileImg, userName: 'Eman Eid' },
+      user: { imgLink: jacksonImg, userName: 'Jackson Mark' },
     },
     {
       rating: 4.3,
-      reviewText: 'very Good restaurant',
-      user: { imgLink: userProfileImg, userName: 'Dina Mahmoud' },
+      reviewText: 'Very Good restaurant',
+      user: { imgLink: mandyImg, userName: 'Laila Mask' },
     },
   ]);
 
   return (
-    <section>
-      <h2>Testimonials</h2>
-      {reviews.map((review) => (
-        <Review key={review.user.userName} {...review} />
-      ))}
+    <section className="bg-accent-dark py-5 mt-5">
+      <div className="container">
+        <h2 className="special-title text-center">Testimonials</h2>
+        <div className="row justify-content-center">
+          {reviews.map((review) => (
+            <div
+              key={review.user.userName}
+              className="col-sm-6 col-md-4 col-lg-3"
+            >
+              <Review {...review} />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
