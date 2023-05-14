77e5bebe5270220d91200ef1b6cd7424f0546e80
AhmedEid3 <ahmed.eid3@outlook.com>
2023-05-14 13:47:21 +0300

    Add ConfirmedBookingPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/layout';
 import HomePage from './pages/home.page';
 import BookingPage from './pages/booking.page';
+import ConfirmedBookingPage from './pages/confirmed-booking.page';
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
-
         <Route path="/booking" element={<BookingPage />} />
+        <Route path="/booking/confirmed" element={<ConfirmedBookingPage />} />
       </Routes>
     </Layout>
   );
---
+++ src/pages/confirmed-booking.page.tsx
@@ -0,0 +1,9 @@
+const ConfirmedBookingPage = () => {
+  return (
+    <main className="container">
+      <p className="lead text-center">The booking has been confirmed! √</p>
+    </main>
+  );
+};
+
+export default ConfirmedBookingPage;
